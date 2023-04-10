b54c7ff4ee4f0d2c2f7c6e7bc2fd0d5050c99d44
Rakib Shahidullah <shsidullarakib@gmail.com>
2023-04-11 03:26:53 +0600

    setup nextJs

---
+++ pages/blog/[blogId].js
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router"
+
+function BlogDetails() {
+    const router = useRouter();
+    const blogId = router.query.blogId;
+
+    console.log(blogId)
+
+    return (
+        <div>This id: {blogId} blog details </div>
+    )
+}
+
+export default BlogDetails
---
+++ pages/blog/index.js
@@ -0,0 +1,12 @@
+function blog() {
+    return (
+        <div>
+            <h4>blog 1</h4>
+            <h4>blog 2</h4>
+            <h4>blog 3</h4>
+            <h4>blog 4</h4>
+        </div>
+    )
+}
+
+export default blog
